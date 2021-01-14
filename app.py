6c4d57f3e7e3c76dd4b875b8de49a752f1e84c03
hekaiyou <hekaiyou@qq.com>
2021-01-14 20:16:11 +0800

    2021/1/14 2016

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 from flask import Flask, request
 from rainbond_python.parameter import Parameter
+from rainbond_python.db_connect import DBConnect
 
 app = Flask(__name__)
+db = DBConnect(db='dragonli', collection='scripts')
 
 
 @app.route('/api/1.0/script', methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -13,7 +15,15 @@ def api_script():
         return json.dumps(parameter.param_url, ensure_ascii=False), 200, []
 
     elif parameter.method == 'POST':
-        return json.dumps(parameter.param_json, ensure_ascii=False), 200, []
+        if parameter.verification(checking=parameter.param_json, verify={'title': str, 'script': str}):
+            param = parameter.param_json
+            insert_dict = {'title': param['title'], 'script': param['script']}
+            if db.write_one_docu(docu=insert_dict):
+                return '新编排剧本被创建', 201, []
+            else:
+                return '编排剧本无法被创建', 500, []
+        else:
+            return '请求参数错误', 400, []
 
     elif parameter.method == 'PUT':
         return json.dumps(parameter.param_json, ensure_ascii=False), 200, []
