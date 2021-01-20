9ce6e4eac9dabbab774daced21518d85a5f54fd6
hekaiyou <hekaiyou@qq.com>
2021-01-20 15:47:30 +0800

    2021/1/20 1547

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ def api_script():
     parameter = Parameter(request)
 
     if parameter.method == 'GET':
-        response = db.find_paging(parameter)
-        return response
+        find_data = db.find_paging(parameter)
+        return find_data, 200, []
 
     elif parameter.method == 'POST':
         param = parameter.verification(checking=parameter.param_json,
                                        verify={'title': str, 'script': str})
         db.write_one_docu(docu=param)
-        return '新编排剧本被创建', 201, [('Content-Type', 'text/plain; charset=utf-8')]
+        return '新编排剧本被创建', 201, []
 
     elif parameter.method == 'PUT':
         return json.dumps(parameter.param_json, ensure_ascii=False), 200, []
